69a08d59c014c42a7ef71a7d11cff95f315d3605
SurajGupta <surajgupta19224@gmail.com>
2024-10-17 21:46:36 +0530

    "Added Header component to App and Home pages, and rearranged route configuration in main.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { useUser } from '@clerk/clerk-react'
 
 function App() {
-  const {user, isLoaded , isSignedIn}= useUser();
+  const { user, isLoaded, isSignedIn } = useUser();
 
-  if(!isSignedIn && isLoaded){
+  if (!isSignedIn && isLoaded) {
     return <Navigate to={'/auth/sign-in'} />
   }
 
   return (
     <>
-      <Outlet/>
+      <Header />
+      <Outlet />
     </>
   )
 }
