bc23982429e79da797a66f5d16263075cede59aa
SurajGupta <surajgupta19224@gmail.com>
2024-10-18 13:12:31 +0530

    "Added @radix-ui/react-dialog dependency and imported its components in AddResume.jsx"

--- src/dashboard/components/AddResume.jsx
+++ src/dashboard/components/AddResume.jsx
@@ -1,14 +1,35 @@
 import { PlusSquare } from 'lucide-react'
 import React from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 
 const AddResume = () => {
-  return (
-    <div>
-        <div className='p-14 py-24 border items-center flex justify-center bg-secondary rounded-lg h-[280px] hover:scale-105 transition-all hover:shadow-md cursor-pointer border-dashed'>
-            <PlusSquare/>
+    return (
+        <div>
+            <div className='p-14 py-24 border items-center flex justify-center bg-secondary rounded-lg h-[280px] hover:scale-105 transition-all hover:shadow-md cursor-pointer border-dashed'>
+                <PlusSquare />
+            </div>
+            <Dialog>
+                <DialogTrigger>Open</DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default AddResume
