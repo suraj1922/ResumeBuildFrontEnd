762d4cbc025760686215d3bb16bbb2d9cccedcd4
SurajGupta <surajgupta19224@gmail.com>
2024-10-19 13:19:42 +0530

    "Added axios and uuid dependencies, updated package-lock.json and package.json, and modified AddResume.jsx component to include uuid and axios functionality"

--- src/dashboard/components/AddResume.jsx
+++ src/dashboard/components/AddResume.jsx
@@ -1,4 +1,4 @@
-import { PlusSquare } from 'lucide-react'
+import { Loader2, PlusSquare } from 'lucide-react'
 import React from 'react'
 import { useState } from 'react'
 import {
@@ -11,9 +11,38 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button'
 import { Input } from "@/components/ui/input"
+import { v4 as uuidv4 } from 'uuid';
+import GlobalApi from './../../../services/GlobalApi'
+import { useUser } from '@clerk/clerk-react'
+// import { title } from 'process'
+// import { error } from 'console'
 
 const AddResume = () => {
     const [openDialog, setOpenDialog] = useState(false) 
+    const [resumeTitle, setresumeTile] = useState();
+    const {user} = useUser();
+    const [loading, setLoading] = useState(false);
+    const onCreate=()=>{
+        setLoading(true);
+        const uuid= uuidv4();
+        const data = {
+            data:{
+                title:resumeTitle,
+                resumeId: uuid,
+                userEmail: user?.primaryEmailAddress?.emailAddress,
+                userName: user?.fullName
+            }
+        }
+
+        GlobalApi.CreateNewResume(data).then(resp=>{
+            console.log(resp)
+            if(resp){
+                setLoading(false);
+            }
+        },(error)=>{
+            setLoading(false)
+        })
+    }
 
     return (
         <div>
@@ -26,11 +55,15 @@ const AddResume = () => {
                         <DialogTitle>Create New Resume</DialogTitle>
                         <DialogDescription>
                             <p>Add a title for your new resume</p>
-                           <Input className='my-2' placeholder='Ex.Full Stack Resume'/>
+                           <Input className='my-2' 
+                           placeholder='Ex.Full Stack Resume'
+                           onChange={(e)=>setresumeTile(e.target.value)}/>
                         </DialogDescription>
                         <div className='flex justify-end gap-5'>
                             <Button onClick={() => setOpenDialog(false)} variant="ghost">Cancel</Button>
-                            <Button>Create</Button>
+                            <Button 
+                            disabled={!resumeTitle || loading}
+                            onClick={()=>onCreate()}> {loading?<Loader2 className='animate-spin'/>:'Create'}</Button>
                         </div>
                     </DialogHeader>
                 </DialogContent>
