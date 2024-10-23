2c67cc68468ec978758cb1e5e90bbd448b43d9e0
SurajGupta <surajgupta19224@gmail.com>
2024-10-23 21:37:36 +0530

    "Refactored Education and Experience components in resume form, updated imports, and added functionality for adding, removing, and saving education and experience entries."

--- src/dashboard/resume/componets/form/Education.jsx
+++ src/dashboard/resume/componets/form/Education.jsx
@@ -1,24 +1,107 @@
-import { Description } from '@radix-ui/react-dialog'
-import { University } from 'lucide-react'
-import React, { useState } from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { toast } from 'sonner'
+import { LoaderCircle } from 'lucide-react'
+import { ResumeInfoContext } from '@/context/ResumeInfoContext'
 const Education = () => {
-
+    const [loading, setLoading] = useState(false)
+    const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext)
     const [educationalList, setEdcationalList] = useState([
         {
-            universityName:'',
-            degree:'',
-            major:'',
-            startDate:'',
-            endDate:'',
-            description:'',
+            universityName: '',
+            degree: '',
+            major: '',
+            startDate: '',
+            endDate: '',
+            description: '',
         }
     ])
+
+    const handleChange = (event, index) => {
+        const newEntries=educationalList.slice();
+        const {name,value}=event.target;
+        newEntries[index][name]=value;
+        console.log(newEntries)
+        setEdcationalList(newEntries); 
+    }
+
+
+    const AddNewEducation = () => {
+        setEdcationalList([...educationalList, {
+            universityName: '',
+            degree: '',
+            major: '',
+            startDate: '',
+            endDate: '',
+            description: '',
+        }])
+    }
+
+    const RemoveEducation = () => {
+        setEdcationalList(educationalList=>educationalList.slice(0,-1))
+    }
+
+    const onSave = () => {
+
+    }
+
+    useEffect(()=>{
+        setResumeInfo({
+            ...resumeInfo,
+            education:educationalList
+        })
+    },[educationalList])
     return (
         <div>
             <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
                 <h2 className='font-bold text-lg'>Education</h2>
                 <p>Add Your Educational Details</p>
+
+                <div>
+                    {educationalList.map((item, index) => (
+                        <div key={index}>
+                            <div className='grid grid-cols-2 gap-3 border p-3 my-5 rounded-lg'>
+                                <div className='col-span-2'>
+                                    <label htmlFor="">University Name</label>
+                                    <Input name='universityName'onChange={(e) => handleChange(e, index)}></Input>
+                                </div>
+                                <div>
+                                    <label htmlFor="">Degree</label>
+                                    <Input name='degree' onChange={(e) => handleChange(e, index)}></Input>
+                                </div>
+                                <div>
+                                    <label htmlFor="">Major</label>
+                                    <Input name='major' onChange={(e) => handleChange(e, index)}></Input>
+                                </div>
+                                <div>
+                                    <label htmlFor="">Start Date</label>
+                                    <Input type="date" name='startDate' onChange={(e) => handleChange(e, index)}></Input>
+                                </div>
+                                <div>
+                                    <label htmlFor="">End Date</label>
+                                    <Input type="date" name='endDate' onChange={(e) => handleChange(e, index)}></Input>
+                                </div>
+                                <div className='col-span-2'>
+                                    <label htmlFor="">Description</label>
+                                    <Textarea name='description' onChange={(e) => handleChange(e, index)}></Textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+                <div className='flex justify-between'>
+                    <div className='flex gap-2'>
+                        <Button variant="outline" onClick={AddNewEducation} className="text-primary"> + Add More Education</Button>
+                        <Button variant="outline" onClick={RemoveEducation} className="text-primary"> - Remove</Button>
+
+                    </div>
+                    <Button disabled={loading} onClick={() => onSave()}>
+                        {loading ? <LoaderCircle className='animate-spin' /> : 'Save'}
+                    </Button>
+                </div>
             </div>
         </div>
     )
